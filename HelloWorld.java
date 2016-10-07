a565d9c466761e5af652c9acc48c3427ad66ff37
ilia-ilia14 <simon.ilia.sd@gmail.com>
2016-10-07 10:48:57 -0600

    I added hello world

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class Helloworld{
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[])
+    {
+	System.out.println("hello world");
+    }
+    
+}
